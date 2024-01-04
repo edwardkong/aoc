6cb0ee2405b909ca9cbc29fd7c3a62c72927b2d4
Edward Kong <eddiekong13@gmail.com>
2024-01-03 23:04:35 -0500

    day19 p1

---
+++ day19/solution.py
@@ -0,0 +1,78 @@
+import re
+
+class WorkflowSorter:
+    def __init__(self, workflows, parts):
+        self.workflows = self.initialize_wf(workflows)
+        self.parts = self.initialize_parts(parts)
+
+    def initialize_wf(self, workflows):
+        wfs = {}
+        for wf in workflows:
+            key, rest = wf.strip('}').split('{')
+            conditions = rest.split(',')
+    
+            wfs[key] = []
+            last = conditions.pop()
+            for c in conditions:
+                pat = r'([a-zA-Z])([<>])(\d+):(\w+)'
+                cat, op, comp, out = re.match(pat, c).groups()
+                wfs[key].append((cat, 
+                                    int.__lt__ if op == '<' else int.__gt__, 
+                                    int(comp), 
+                                    out))
+            wfs[key].append(last)
+        return wfs
+    
+    def initialize_parts(self, parts):
+        processed = []
+        for part in parts:
+            vals = {}
+            for category in part[1:-1].split(','):
+                ch, val = category.split('=')
+                vals[ch] = int(val)
+            processed.append(vals)
+        return processed
+    
+    def process_items(self):
+        wf = self.workflows
+        p = self.parts
+        res = 0
+        for part in p:
+            rule = 0
+            key = 'lqg' if part['a'] > 2381 else 'kt'
+            while True:
+                process = wf[key]
+                matched = False
+                for rule in process[:-1]:
+                    cat, op, val, dest = rule
+                    if op(part[cat], val):
+                        key = dest
+                        matched = True
+                        break
+                if not matched:
+                    key = process[-1]
+                if key == 'R':
+                    break
+                elif key == 'A':
+                    res += sum(part.values())
+                    break
+                
+        return res
+                        
+
+
+
+
+def main_part_one():
+    with open('day19/input.txt', 'r') as file:
+        workflows, parts  = [x.splitlines() for x in file.read().split('\n\n')]
+    wfs = WorkflowSorter(workflows, parts)
+    return wfs.process_items()
+
+def main_part_two():    
+    with open('day18/input.txt', 'r') as file:
+        pass
+
+if __name__ == '__main__':
+    print(main_part_one())
+    print(main_part_two())
