093d38367ebc07c3f64d08b84df072ddcfd0f0ac
Edward Kong <eddiekong13@gmail.com>
2024-01-08 22:28:46 -0500

    day21 solved

--- day21/solution.py
+++ day21/solution.py
@@ -1,100 +1,115 @@
-from functools import cache
-import numpy as np
-import scipy.sparse as sp
+from collections import deque
 
 class Garden:
     def __init__(self, grid):
         self.grid = grid
         self.h = len(self.grid)
         self.w = len(self.grid[0])
+        self.s = self.get_starting_square()
 
     def get_starting_square(self):
         for r in range(self.h):
             if 'S' in self.grid[r]:
                 return r, self.grid[r].index('S')
 
-    @cache
-    def get_accessible_neighbors(self, row, col):
-        adj = []
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            nr, nc = dr + row, dc + col
-            if 0 <= nr < self.h and 0 <= nc < self.w:
-                if self.grid[nr][nc] == '#':
+    def visit_n(self, n, row=None, col=None):
+        if row is None or col is None:
+            row, col = self.s
+        res = set()
+        seen = {(row, col)}
+        q = deque([(row, col, n)])
+        while q:
+            r, c, steps = q.popleft()
+
+            if steps % 2 == 0:
+                res.add((r, c))
+            if steps == 0:
+                continue
+
+            for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+                if (not (0 <= nr < self.h) or
+                    not (0 <= nc < self.w) or 
+                    self.grid[nr][nc] == "#" or 
+                    (nr, nc) in seen):
                     continue
-                else:
-                    adj.append((nr, nc))
-        return adj
+                seen.add((nr, nc))
+                q.append((nr, nc, steps - 1))
+        
+        return len(res)
 
-    def initialize_matrix(self):
-        n = self.h * self.w
-        adj_matrix = np.zeros((n, n), dtype=int)
-        for r in range(self.h):
-            for c in range(self.w):
-                for edge in self.get_accessible_neighbors(r, c):
-                    adj_matrix[r * self.w + c][edge[0] * self.w + edge[1]] = 1
-        return adj_matrix
+    def visit_all(self, parity=None):
+        seen = set()
+        parity_seen = set()
+        q = deque([(self.s[0], self.s[1], 1)])
+        while q:
+            r, c, steps = q.popleft()
+            for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+                if (not (0 <= nr < self.h) or
+                    not (0 <= nc < self.w) or 
+                    self.grid[nr][nc] == "#" or
+                    (nr, nc) in seen):
+                    continue
+                if ((parity == 'odd' and steps % 2 == 1) or
+                    (parity == 'even' and steps % 2 == 0)):
+                        parity_seen.add((nr, nc))
+                seen.add((nr, nc))
+                q.append((nr, nc, steps + 1))
+        if parity is None:
+            return len(seen)
+        else:
+            return len(parity_seen)
+    
+    def calc_area(self, n):
+        l = self.w
+        grids = n // l - 1
+        r, c = self.s
 
-    def get_positions_after_n_moves(self, n):
-        sq_r, sq_c = self.get_starting_square()
-        sq = sq_r * self.w + sq_c
-        adj = self.initialize_matrix()
-        sparse_matrix = sp.csr_matrix(adj)
-        matrix_pow = sparse_matrix ** (n)
-        dense_matrix_pow = matrix_pow.toarray()
-        return np.count_nonzero(dense_matrix_pow[sq])
-    
-    """
-    I tried taking a matrix eponentiation approach to count the nonzero indices
-    and finding a cycle however hashing the 17161x17161 matrix is quite slow (I
-    tried to use the a tuple of the indices of nonzero elements for my key)
-    and I didn't realize the prompt mentioned the map repeats infinitely.
-    So this solution is a dead end, but I'd like to revisit the concept of
-    hashing sparse matrices later on as well as run benchmarks on the same
-    computations between sparse and dense matrices. Mathematically, dense 
-    matrices store 8 bytes per entry while sparse matrices store 12 bytes per
-    entry, so there is a theoretical 67% sparsity threshold below which sparse
-    matrices should be more memory and compute efficient, without considering 
-    for interpreter/translation overhead.
-    
-    def find_cycle(self, n):
-        sq_r, sq_c = self.get_starting_square()
-        sq = sq_r * self.w + sq_c
-        adj = self.initialize_matrix()
-        sparse_end_matrix = sparse_adj_matrix = sp.csr_matrix(adj)
-        seen = {}
-        step = 1
-        while True:
-            step += 1
-            sparse_end_matrix = sparse_end_matrix.astype(bool).dot(sparse_adj_matrix)
-            rows, cols = sparse_end_matrix.nonzero()
-            non_zeros = 0
-            #non_zeros = tuple((row, col) for row, col in zip(rows, cols))
-            print(step)
-            if non_zeros in seen:
-                print(step)
-                cycle = step - seen[non_zeros]
-                break
-            else:
-                #seen[non_zeros] = step
-                pass
-        steps_left = (n - seen[non_zeros]) % cycle
-        matrix_pow = sparse_adj_matrix ** (steps_left)
-        dense_matrix_pow = matrix_pow.toarray()
-        return np.count_nonzero(dense_matrix_pow[sq])
-        """
+        def sum_partials(entry, count, steps):
+            """
+            Sums the squares visited in each type of partially filled grid.
+            Args:
+                entry (list[int]): The closest point to the center per grid.
+                count (int): The amount of each type of grid.
+                steps (int): The length travelled into each type of grid.
+
+            Returns:
+                int: The total number of squares visited for each type of grid
+            """
+            return sum(self.visit_n(steps - 1, r, c) for r, c in entry) * count
+
+        # Number of each parity grid
+        odd_grids = (grids // 2 * 2 + 1) ** 2
+        even_grids = ((grids + 1) // 2 * 2) ** 2
+
+        # Number of squares in each parity grid
+        sq_in_odd = self.visit_all(parity='odd')
+        sq_in_even = self.visit_all(parity='even')
+
+        odds, evens = odd_grids * sq_in_odd, even_grids * sq_in_even
+
+        # Entry points for corners and edges
+        corner_pat = [(l - 1, c), (r, 0), (0, c), (r, l - 1)]
+        edge_pat = [(l - 1, 0), (0, 0), (0, l - 1), (l - 1, l - 1)]
+
+        # Get all squares in partially explored grids
+        corners = sum_partials(corner_pat, 1, l)
+        sm_edge = sum_partials(edge_pat, grids + 1, l // 2)
+        lg_edge = sum_partials(edge_pat, grids, l * 3 // 2)
+
+        return odds + evens + corners + sm_edge + lg_edge
 
 def main_part_one():
     with open('day21/input.txt', 'r') as file:
         lines = file.read().splitlines()
     g = Garden(lines)
-    return g.get_positions_after_n_moves(64)
+    return g.visit_n(64)
 
 def main_part_two():    
     with open('day21/input.txt', 'r') as file:
         lines = file.read().splitlines()
     g = Garden(lines)
-    return g.find_cycle(26501365)
+    return g.calc_area(26501365)
 
 if __name__ == '__main__':
-    #print(main_part_one())
+    print(main_part_one())
     print(main_part_two())
